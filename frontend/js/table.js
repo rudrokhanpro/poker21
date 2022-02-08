f40fae4220876942db9eee7366be99365c31122e
Kassim <kassim91000@gmail.com>
2022-02-08 11:05:24 +0100

    init deck logger

--- frontend/js/table.js
+++ frontend/js/table.js
@@ -45,5 +45,7 @@ drawBtn.onclick = async (event) => {
 
 	document.getElementById('card').setAttribute('src', cards[0].image);
 
+	document.getElementById('logger').setAttribute('value', cards[0].value);
+
 	cards.forEach((card) => player_deck.push(card));
 };
