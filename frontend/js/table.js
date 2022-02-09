ff87446850c5422941925401719676e3f1e06b89
Kassim <kassim91000@gmail.com>
2022-02-09 09:53:50 +0100

    dévelopemment du logger

--- frontend/js/table.js
+++ frontend/js/table.js
@@ -45,7 +45,14 @@ drawBtn.onclick = async (event) => {
 
 	document.getElementById('card').setAttribute('src', cards[0].image);
 
-	document.getElementById('logger').setAttribute('value', cards[0].value);
+	
+	var value = document.getElementById('logger').innerText= "Vous avez eu la carte " + ('value', cards[0].value);
 
+	var p=document.getElementById("console");
+	var e=document.createElement("div");
+	e.innerHTML="Element n°"+ value;
+	p.appendChild(e);
+
+		
 	cards.forEach((card) => player_deck.push(card));
 };
