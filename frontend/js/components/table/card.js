c069c6f0f901cb8a3b8ff0f0c920dca4e519fa3d
Steven TAING <taing.steven@outlook.fr>
2022-04-03 16:48:23 +0200

    Merge remote-tracking branch 'origin/keyboard-action' into fix-conflits

---
+++ frontend/js/components/table/card.js
@@ -0,0 +1,38 @@
+export default class Card {
+	constructor(card, parent = document.body) {
+		/*
+            "image": "https://deckofcardsapi.com/static/img/KH.png",
+            "value": "KING",
+            "suit": "HEARTS",
+            "code": "KH"
+        */
+		this.value = card.value;
+		this.suit = card.suit;
+		this.code = card.code;
+		this.image = card.image;
+
+        this.parent = parent;
+
+        this.el = document.createElement('div');
+        this.visible = false;
+
+		this.#setup();
+	}
+
+    display() {
+        this.el.classList.add('card--visible');
+    }
+
+	#setup() {
+		this.el.setAttribute('id', `card-${this.value}-${this.suit}`);
+        this.el.classList.add('card');
+
+        const imageEl = document.createElement('img');
+        imageEl.setAttribute('src', this.image);
+
+        imageEl.classList.add('card__image');
+
+        this.el.appendChild(imageEl);
+        this.parent.appendChild(this.el);
+	}
+}
