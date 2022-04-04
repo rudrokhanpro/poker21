c71e1312e560ce8c479a0b38f5f95501e68f8cc3
Rudro Khan <rudrokhan.pro@gmail.com>
2022-04-04 10:03:31 -0700

    fix: Fix of missing import

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,19 +1,20 @@
 import Loader from './components/loader.js';
 import { createRelativeURL } from './utils/helpers.js';
+import { newShuffle } from './utils/api.js';
 
 let loader = new Loader('loader');
 
 const gameButton = document.querySelector('.start');
 
-gameButton.onclick =  async () => {
+gameButton.onclick = async () => {
 	loader.display();
 	let current_id = localStorage.getItem('deck_id');
 
-	if(!current_id) {
+	if (!current_id) {
 		const newDeck = await newShuffle({ deck_count: 1 });
 		let current_id = newDeck.deck_id;
 		localStorage.setItem('deck_id', current_id);
 	}
 
-	window.location.href = createRelativeURL('/table.html', {current_id});
-}
+	window.location.href = createRelativeURL('/table.html', { current_id });
+};
